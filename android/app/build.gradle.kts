b38e93519acfcabb78136938fae57d415ff82c2f
hongheuijae-blip <honghejae@gmail.com>
2026-07-14 00:36:35 +0000

    Add Compose Foundation dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.compose.foundation:foundation")
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-firestore-ktx")
